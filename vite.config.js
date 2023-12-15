a784f69133fb8ffa78ce82c4534289557d204fdc
dato korgalidze <d.korgalidze@gmail.com>
2023-12-15 15:15:24 +0400

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react/dist/index.mjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+
 
 
 

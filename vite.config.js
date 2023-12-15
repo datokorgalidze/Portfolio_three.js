79ad9da1ceed7ccc3ebcfd5ec709f9564df47cfb
dato korgalidze <d.korgalidze@gmail.com>
2023-12-15 14:31:26 +0400

    my last commit here

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  react: {
-    version: "^18.2.0",
-  },
-  server: {
-    port: process.env.PORT || 5173,
-  },
-});
+})
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   react: {
+//     version: "^18.2.0",
+//   },
+//   server: {
+//     port: process.env.PORT || 5173,
+//   },
+// });
